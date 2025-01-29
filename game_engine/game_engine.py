4c3ebe1f95a51c2182f251264aec131d18b8c19f
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-29 09:25:11 +0700

    Add game Todo list

--- game_engine/game_engine.py
+++ game_engine/game_engine.py
@@ -1,7 +1,10 @@
 """
 TODO:
     Player (snake)
+        can bite other player tail make them shoter and decrease score
     Object (apple)
+        normal_apple: coin 
+        special_apple: can shoot lazer from the eyes
 
     Game components:
         Game State:
--- game_engine/player.py
+++ game_engine/player.py
@@ -1,17 +1,53 @@
 import numpy as np
-from typing import Tuple
+from typing import Tuple, List
 import logging
 import pygame
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-class Snake:
-    def __init__(self, head: Tuple[int, int], segment_num: int=7) -> None:
-        self.head = head
-        self.segment = segment_num
+"""
+TODO:
+    adding angle constant
+    edit the Snake and Testit
+"""
 
-############### Implement simple procedural animation ##############
+class Snake:
+    # Constant value
+    SEGMENT_SPACE = 20
+
+    def __init__(self, head: List[int], segment_num: int=3) -> None:
+        self.head = head
+        self.segment = [[0, 0] for _ in range(segment_num)]
+        self.segment_raduis = [5-i for i in range(segment_num+1)]
+
+    def maintain_point(self, 
+                       point1: List[int], 
+                       point2: List[int]) -> List[int]:
+        
+        dx, dy = point2[0] - point1[0], point2[1] - point1[1]
+        current_distance = np.sqrt(dx**2 + dy**2)
+
+        if current_distance != self.SEGMENT_SPACE:
+            scale = self.SEGMENT_SPACE / current_distance  
+            point2[0] = point1[0] + dx * scale
+            point2[1] = point1[1] + dy * scale
+
+        return point2
+    
+    def maintain_distance(self) -> None:
+        self.segment[0] = self.maintain_point(self.head, self.segment[0])
+        
+        for i in range(1, len(self.segment)):
+            self.segment[i] = self.maintain_point(self.segment[i-1], self.segment[i])
+
+    def get_segment_postion(self) -> List[int]:
+        pass
+
+    def get_segment_radius(self) -> List[int]:
+        return self.segment_raduis
+
+############### Testing Implement simple procedural animation ##############
 class Testing:
     WIDTH, HEIGHT = 800, 600
     SEGMENT_RADIUS = 50
@@ -21,8 +57,10 @@ class Testing:
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.clock = pygame.time.Clock()
         
-        self.head = pygame.Vector2(self.WIDTH // 2, self.HEIGHT // 2)  # Fixed point
-        self.segment = [pygame.Vector2(0, 0) for _ in range(5)]
+        self.player = Snake(
+            head=(self.WIDTH//2, self.HEIGHT//2)
+        )
+        
 
     def run(self) -> None:
         running = True
@@ -32,7 +70,7 @@ class Testing:
                     running = False
 
             # Update the position of point_b and maintain the distance
-            self.maintain_distance()
+            self.player.maintain_distance()
 
             # Render everything
             self.screen.fill((0, 0, 0))
@@ -43,30 +81,34 @@ class Testing:
         pygame.quit()
 
     def draw(self):
-        pygame.draw.circle(self.screen, (255, 0, 0), (int(self.head.x), int(self.head.y)), radius=10)
+        segment_radius = self.player.get_segment_radius() 
+        segment_position = self.player.get_segment_postion()
+
+        for i in range(len(segment_radius)):
+            pygame.draw.circle(self.screen, 
+                               (0, 255, 0), 
+                               ((int(segment_position[i][0]), int(segment_position[i][1]))), 
+                               radius=segment_radius[i])
+
+    # def maintain_distance(self):
+    #     mouse_pos = pygame.mouse.get_pos() 
+    #     self.head = pygame.Vector2(mouse_pos[0], mouse_pos[1])  
         
-        for i in range(len(self.segment)):
-            pygame.draw.circle(self.screen, (0, 255, 0), ((int(self.segment[i].x), int(self.segment[i].y))), radius=50)
-
-    def maintain_distance(self):
-        mouse_pos = pygame.mouse.get_pos() 
-        self.head = pygame.Vector2(mouse_pos[0], mouse_pos[1])  
+    #     self.segment[0] = self.maintain_point(self.head, self.segment[0])
         
-        self.segment[0] = self.maintain_point(self.head, self.segment[0])
-        
-        for i in range(1, len(self.segment)):
-            self.segment[i] = self.maintain_point(self.segment[i-1], self.segment[i])
+    #     for i in range(1, len(self.segment)):
+    #         self.segment[i] = self.maintain_point(self.segment[i-1], self.segment[i])
 
-    def maintain_point(self, point1: pygame.Vector2, point2: pygame.Vector2) -> pygame.Vector2:
-        dx, dy = point2.x - point1.x, point2.y - point1.y
-        current_distance = np.sqrt(dx**2 + dy**2)
+    # def maintain_point(self, point1: pygame.Vector2, point2: pygame.Vector2) -> pygame.Vector2:
+    #     dx, dy = point2.x - point1.x, point2.y - point1.y
+    #     current_distance = np.sqrt(dx**2 + dy**2)
 
-        if current_distance != self.SEGMENT_RADIUS:
-            scale = self.SEGMENT_RADIUS / current_distance  
-            point2.x = point1.x + dx * scale
-            point2.y = point1.y + dy * scale
+    #     if current_distance != self.SEGMENT_RADIUS:
+    #         scale = self.SEGMENT_RADIUS / current_distance  
+    #         point2.x = point1.x + dx * scale
+    #         point2.y = point1.y + dy * scale
 
-        return point2
+    #     return point2
 
 if __name__ == "__main__":
     test = Testing()
