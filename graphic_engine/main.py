7b0c297b0dfbf8c417ddd4675b76f8e72881e4b2
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-30 03:21:23 +0700

    goofy ah snake 1

--- graphic_engine/main.py
+++ graphic_engine/main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class PlayerGraphicBasic:
     def __init__(self, chain_position: list[tuple[float, float, float]], screen: pygame.Surface, color: str = "aliceblue") -> None:        
@@ -7,8 +8,42 @@ class PlayerGraphicBasic:
         self.color = color
 
     def body_draw(self):
-        for bone in self.skeleton:
+        skeleton_vectors = []
+        left_vectors = []
+        right_vectors = []
+        for i, bone in enumerate(self.skeleton):
             pygame.draw.circle(surface=self.screen, color=self.color, center=(bone[0], bone[1]), radius=bone[2])
+            print(i)
+
+            if i <= len(self.skeleton) - 2:
+                print(i)
+                direction = math.atan(self.skeleton[i+1][1] - bone[1])/(self.skeleton[i+1][0])
+
+
+            bone_left = (math.cos(direction + math.pi/2) * bone[2] + bone[0],
+                         math.sin(direction + math.pi/2) * bone[2] + bone[1])
+            bone_right = (math.cos(direction - math.pi/2) * bone[2] + bone[0],
+                          math.sin(direction - math.pi/2) * bone[2] + bone[1])
+
+            if i >= 1:
+                skeleton_vectors.append(((bone[0], bone[1]),(previous_bone[0], previous_bone[1])))
+                pygame.draw.line(surface=self.screen, color=self.color, start_pos=previous_bone_left, end_pos=bone_left)
+                pygame.draw.line(surface=self.screen, color=self.color, start_pos=previous_bone_right, end_pos=bone_right)
+                
+                if i == 1:
+                    pygame.draw.circle(surface=self.screen, color="white", center=previous_bone_left, radius=15)
+                    pygame.draw.circle(surface=self.screen, color="white", center=previous_bone_right, radius=15)
+                    pygame.draw.circle(surface=self.screen, color="black", center=previous_bone_left, radius=10)
+                    pygame.draw.circle(surface=self.screen, color="black", center=previous_bone_right, radius=10)
+
+            previous_bone = (bone[0], bone[1], bone[2])
+            previous_bone_left = (math.cos(direction + math.pi/2) * previous_bone[2] + previous_bone[0],
+                                  math.sin(direction + math.pi/2) * previous_bone[2] + previous_bone[1])
+            previous_bone_right = (math.cos(direction - math.pi/2) * previous_bone[2] + previous_bone[0],
+                                   math.sin(direction - math.pi/2) * previous_bone[2] + previous_bone[1])
+
+            
+
 
         
 
@@ -62,7 +97,7 @@ if __name__ == "__main__":
     EXAMPLE_CHAIN = [(100, 150, 50), (200, 170, 45), (300, 180, 35), (400, 200, 20)]
     PLAYER1 = PlayerGraphicBasic(EXAMPLE_CHAIN, GAME.screen)
     PLAYER_LST = [PLAYER1]
-    FRUITS_LST = Fruits([(6, 9), (20, 12), (32, 64), (100, 200)], GAME.screen)
+    FRUITS_LST = Fruits([(6, 9), (20, 12), (32, 64)], GAME.screen)
 
     GAME.reg_players(PLAYER_LST)
     GAME.reg_fruits(FRUITS_LST)
